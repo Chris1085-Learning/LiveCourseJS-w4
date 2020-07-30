d3910c831a7b9dc1e54b51ab9d5d5974823be449
chris1085 <itscfchiang@gmail.com>
2020-07-30 15:20:36 +0800

    Add api.js to gitignore

--- js/api.js
+++ js/api.js
@@ -1,8 +1,8 @@
 // const { default: Axios } = require("axios");
 
-const uuid = "017c2859-74ba-4b94-af09-246e50d8864f";
-const token = "pzCKQorsn9wlBBC8XL7nKf73NA2KM5CrlJ0NDfC30ey6CujDk0cUnerzgeQU";
-let apiPath = "https://course-ec-api.hexschool.io/";
+const uuid = '';
+const token = '';
+let apiPath = 'https://course-ec-api.hexschool.io/';
 let sampleData = {};
 
 Axios.defaults.headers.Authorization = `Bearer ${token}`;
